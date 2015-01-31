4e325aebf551a878a1ce428a79a92f95a6cb54d9
Crystal Humphries <CrystalHumphries@users.noreply.github.com>
2015-01-30 16:50:18 -0800

    Update diffExpMeth.R
    -changed Covarlist to Covariate vector.
    -interpret covariate list length

--- src/diffExpMeth.R
+++ src/diffExpMeth.R
@@ -1,13 +1,13 @@
-diffExpMeth<-function(design, dataMatrix, CovList, ...){
+diffExpMeth<-function(design, dataMat, covarVec, ...){
    require(limma)
   
-  FixedDataMatrix<-dataMatrix[, colnames(dataMatrix) %in% rownames(design)]
+  FixedDataMatrix<-dataMat[, colnames(dataMat) %in% rownames(design)]
   
   #create file name for data output
   DataType       = strsplit(rownames(FixedDataMatrix)[1], split=":")[[1]][3]
   OutputFile     = paste0("DE_", DataType)
   OutputFileName = add_date_tag(OutputFile, ".txt")
-  writingDir           = "./"
+  writingDir     = "./"
   OutputFile_dir = paste0(writingDir, OutputFileName)
   
   #make sure that the samples are the same in both the dataMatrix and design matrix
@@ -20,7 +20,7 @@ diffExpMeth<-function(design, dataMatrix, CovList, ...){
     fit1<-eBayes(fit1)
     
     #gather relevant covariates
-    cov_length = (length(CovList) + 1)
+    cov_length = (length(covarVec) + 1)
     DesignVariables  = dim(design)[2]
     Coef_of_Interest = paste(cov_length,DesignVariables, sep=":")
     
@@ -31,16 +31,14 @@ diffExpMeth<-function(design, dataMatrix, CovList, ...){
   }
   
   else{
-  
       stop("Sample IDs in Design Matrix do not match Sample IDs in Data Matrix")
-  
   }
 }
 
 add_date_tag <- function(stringToTag, fileExtension){
     today <- Sys.Date()
     todayf <- format(today, format="%Y%m%d")
-    return(paste(stringToTag, "_",todayf, sep = "", fileExtension))
+    return(paste(stringToTag, "_", todayf, sep = "", fileExtension))
 }
       
     
