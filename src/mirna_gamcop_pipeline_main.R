5fbbbe1a4fbeb872aaece4f62ff94e200f4c0de2
nchambwe <nchambwe@systemsbiology.org>
2015-03-09 14:56:21 -0700

    automated DE analysis for miRNA - regular or bootstrap version

--- src/mirna_gamcop_pipeline_main.R
+++ src/mirna_gamcop_pipeline_main.R
@@ -1,8 +1,12 @@
 # Script to run miRNA-Seq differential expression analysis using the unified GAMCOP Pipeline
-# Date: 02/02/2015
-# Author: Nyasha Chambwe
+# Date: 03/03/2015
+# Author: Nyasha Chambwe  (derived from Dave's Methylation Pipeline)
+
+######################################################################################################################
 
 # Source Functions and R Packages -----------------------------------------
+setwd("/Volumes/~nchambwe/inova/projects/gamcop/gamcop_pipeline")
+
 # Source functions from: https://github.com/Gibbsdavidl/gamcop_pipeline.git
 # Developed in collaboration between Crystal Humphries, Dave Gibbs and Nyasha Chambwe
 source("src/diffExprFun.R")
@@ -23,170 +27,111 @@ filter_diffExp <- function(pathToDeTable, FCThresh, pValueThresh){
   return(diffExpTab)
 }
 
+mapToValidatedTargetGenes <- function(deTable){
+  val_targets <- validated_targets[,c("mature_mirna_id", "target_symbol", "target_entrez", "target_ensembl", "database")]
+  dat <- unique(merge(deTable, val_targets, by.x = "mirID", by.y="mature_mirna_id", all.x = T))
+  return(dat)
+}
+
+mapToPredictedTargetGenes <- function(deTable){
+  pred_targets <- predicted_targets[,c("mature_mirna_id", "target_symbol")]
+  dat <- unique(merge(deTable, pred_targets, by.x = "mirID", by.y="mature_mirna_id" ))
+  return(dat)
+}
+######################################################################################################################
+
 # Load Input Files --------------------------------------------------------
+# Load data
+setwd("/Volumes/~nchambwe/inova/projects/gamcop/gamcop_pipeline")
 
-# Load Input Files
+# Validated miRNA Target Genes
+validated_targets <- read.table("data/miRNA-annotations/multiMir_validated_targets_20150213.txt")
 # Clinical data file
-clinDat <-  read.table("/Volumes/~nchambwe/inova/projects/gamcop/smrna-analysis/data/clinical/data_CLIN_20150203.fm", 
-                       check.names=F, header=T, row.names = 1)
-molDat <- read.table("/Volumes/~nchambwe/inova/projects/gamcop/smrna-analysis/data/molecular/small_rna_seq/Study_101/batch_summaries/data_MIRSeq_LIMMA_corrected_20141201.fm", 
-                     header=T, sep="\t", row.names=1, check.names = F)
-#"B:M:CLIN:Critical_Phenotype:Preeclampsia/Eclampsia",
-targetPhenotypes <- c("B:NB:CLIN:Critical_Phenotype:1n2v4",
-                      "B:NB:CLIN:Critical_Phenotype:1v4",
-                      "N:NB:CLIN:Critical_Phenotype:Gestational_Age_at_Delivery",
-                      "B:NB:MRGE:Critical_Phenotype:History_of_Preterm_Birth",
-                      "B:NB:MRGE:Critical_Phenotype:Hypertension_Related",
-                      "B:NB:MRGE:Critical_Phenotype:Immune_Related",
-                      "B:M:CLIN:Critical_Phenotype:Incompetent_Shortened_Cervix",
-                      "B:M:CLIN:Critical_Phenotype:Inova_Idiopathic_NA",
-                      "B:NB:MRGE:Critical_Phenotype:Placenta_Related",
-                      "B:NB:CLIN:Critical_Phenotype:Preterm",
-                      "B:NB:MRGE:Critical_Phenotype:Prom_Related",
-                      "N:NB:CLIN:Critical_Phenotype:TermCategory",
-                      "B:NB:MRGE:Critical_Phenotype:Uterine_Related"
-)
+#clinDat <-  read.table("data/clinical/data_CLIN_20150203.fm", check.names=F, header=T, row.names = 1, stringsAsFactors = F)
+clinDat <-  read.table("data/clinical/data_CLIN_20150303.fm", check.names=F, header=T, row.names = 1, stringsAsFactors = F)
 
-covariates = c("N:F:SURV:Data:Date_of_Birth__relative_to_Date_of_Birth", "C:F:ADMX:Data:Admix_80_Percent") 
+molDat <- read.table("data/molecular/small_rna_seq/Study_101/batch_summaries/data_MIRSeq_LIMMA_corrected_20141201.fm", header=T, sep="\t", row.names=1, check.names = F)
+validated_targets <- read.table("data/miRNA-annotations/multiMir_validated_targets_20150213.txt")
 
-# Differential Expression Analysis: All Samples ---------------------------
-foldChange <- 0.25
+# subset clinical matrix for samples with available molecular data
+clinDat <- clinDat[,(colnames(clinDat)  %in% colnames(molDat))]
+
+# Fix forward slash in Preeclampsia/Eclampsia
+idx <- grep(rownames(clinDat), pattern = "Preec")
+rownames(clinDat)[idx] <- "B:M:CLIN:Critical_Phenotype:Preeclampsia_Eclampsia"
+
+targetPhenotypes <- row.names(clinDat)[grep(pattern = "Critical_Phenotype", x = row.names(clinDat), ignore.case = T)]
+
+# old dates
+#bloodDrawDates <- as.numeric(clinDat["N:M:CLIN:Data:Date_of_Blood_Collection__relative_to_Date_of_Birth", ])
+# new Dates
+bloodDrawDates <- as.numeric(clinDat["N:M:CLIN:Data:Date_of_Blood_Collection_20150223__relative_to_Date_of_Birth",])
+foldchange <- 0.25
 pvalue <- 0.05
-resultsLocation <- "/Volumes/~nchambwe/inova/projects/gamcop/gamcop_pipeline/results/diffExpAnalysis/20150216/all_samples/"
-gamcop<- targetPhenotypes
 
-sapply(gamcop, function(x) diffExprFun(clinMat = clinDat, dataMat=molDat, targetPheno=x, 
-                                       covarVec=covariates, FCThresh=foldChange, pValueThresh=pvalue, 
-                                       writingDir=resultsLocation))
+#####################################################################################################################
 
-diffExpTables <- list.files(path = resultsLocation, pattern="DE_MIRNA_*", all.files = F, recursive = T)
-print(diffExpTables)
+dayString <- c("AllDays","04Days","1Days")
+today <- format(Sys.Date(), format="%Y%m%d")
+dirs <- vector()
+root <- paste0("results/diffExpAnalysis/", today)
+dir.create(path = root, showWarnings = T)
 
-for(i in 1:length(gamcop)){
-  sigMirsTable <- filter_diffExp(pathToDeTable =paste0(resultsLocation, diffExpTables[i]), FCThresh = foldChange, pValueThresh = pvalue)
-  mirs <- row.names(sigMirsTable)
-  comparison <- unlist(str_split(gamcop[i], pattern = ":"))[[5]]
-  print(paste0("The number of differentially expressed mirs for ", comparison, " is: ", length(mirs)))
+for(i in 1:length(dayString)){
+  result <-  paste0(root, "/DE_blood_draw_", dayString[i])
+  dir.create(path = result, showWarnings = T)
+  dirs[i] <- result
+}
+
+for (day in 1:3) {
+  if (day == 1) {
+    # then take them all 
+    idx <- bloodDrawDates > -100000
+    covariates <- c("N:M:SURV:Data:Date_of_Birth__relative_to_Date_of_Birth", 
+                    "C:M:ADMX:Data:Admix_80_Percent",
+                    "N:M:CLIN:Data:Date_of_Blood_Collection__relative_to_Date_of_Birth")
+  } else if (day == 2) {
+    idx <- bloodDrawDates >= 0 & bloodDrawDates <= 4
+    covariates <- c("N:M:SURV:Data:Date_of_Birth__relative_to_Date_of_Birth", 
+                    "C:M:ADMX:Data:Admix_80_Percent",
+                    "N:M:CLIN:Data:Date_of_Blood_Collection__relative_to_Date_of_Birth")
+  } else if (day == 3) {
+    idx <- bloodDrawDates == 1
+    covariates <- c("N:M:SURV:Data:Date_of_Birth__relative_to_Date_of_Birth", 
+                    "C:M:ADMX:Data:Admix_80_Percent")
+  }
+  idx[is.na(idx)] <- F
+  clinMatFilt <- clinDat[,idx]  
+  outdir <- dirs[day]; 
   
-  if(length(mirs)>0){
-    filename <- paste0(resultsLocation, "sigMirList_", diffExpTables[i])
-    write.table(x = mirs, file = filename, quote = F, col.names = F,row.names=F)
+  for (target in targetPhenotypes) {
+    targetString <- str_split(target, ":")[[1]][5]; #print(targetString)
+    deTable <- bootDiffFun(clinMat=clinMatFilt, dataMat=molDat,  targetPheno=target,
+                           covarVec=covariates, FCThresh=foldchange, pValueThresh=pvalue,
+                           writingDir=outdir, reps=1000, cpus=1, robustFlag=F, writeTable=F)
     
-    get_validated_mir_targets(mirOfInterest = mirs, targetPhenotypeName =comparison , 
-                              writingDir =resultsLocation)
-  }
-  print("Completed: miRNA Target identification")
-}
-
-# Differential Expression Analysis: All Samples - BloodDraw Covariates ---------------------------
-print("Differential Expression Analysis: All Samples - BloodDraw Covariates ---------------------------")
-foldChange <- 0.25
-pvalue <- 0.052
-covariates = c("N:F:SURV:Data:Date_of_Birth__relative_to_Date_of_Birth", "C:F:ADMX:Data:Admix_80_Percent", 
-               "N:M:CLIN:Data:Date_of_Blood_Collection__relative_to_Date_of_Birth") 
-
-resultsLocation <- "/Volumes/~nchambwe/inova/projects/gamcop/gamcop_pipeline/results/diffExpAnalysis/20150216/all_samples_blood_draw/"
-
-gamcop<- targetPhenotypes
-
-sapply(gamcop, function(x) diffExprFun(clinMat = clinDat, dataMat=molDat, targetPheno=x, 
-                                       covarVec=covariates, FCThresh=foldChange, pValueThresh=pvalue, 
-                                       writingDir=resultsLocation))
-
-diffExpTables <- list.files(path = resultsLocation, pattern="DE_MIRNA_*", all.files = F, recursive = T)
-print(diffExpTables)
-
-
-for(i in 1:length(gamcop)){
-  sigMirsTable <- filter_diffExp(pathToDeTable =paste0(resultsLocation, diffExpTables[i]), FCThresh = foldChange, pValueThresh = pvalue)
-  dim(sigMirsTable)
-  mirs <- row.names(sigMirsTable)
-  comparison <- unlist(str_split(gamcop[i], pattern = ":"))[[5]]
-  print(paste0("The number of differentially expressed mirs for ", comparison, " is: ", length(mirs)))
-  
-  if(length(mirs)>0){
-    filename <- paste0(resultsLocation, "sigMirList_", diffExpTables[i])
-    write.table(x = mirs, file = filename, quote = F, col.names = F,row.names=F)
+    #deTable <- diffExprFun(clinMat = clinMatFilt, dataMat=molDat, targetPheno=target, 
+    #                       covarVec=covariates, FCThresh=foldchange, pValueThresh=pvalue, 
+    #                       writingDir=outdir, robustFlag=F, writeTable=false)
     
-    get_validated_mir_targets(mirOfInterest = mirs, targetPhenotypeName =comparison , 
-                              writingDir =resultsLocation)
+    deTable$mirID <- do.call("rbind", lapply(str_split(rownames(deTable), ":"), "[[", 5))
+    
+    print("*****************")
+    print(paste0("Phenotype: ", targetString)) 
+    print(paste0("Blood Draw Dates: ", dayString[day]))
+    pos <- length(which(clinMatFilt[target,] == "true"))
+    neg <- length(which(clinMatFilt[target,] == "false"))
+    tot <-  ncol(clinMatFilt[target,])
+    print(paste0("Number of samples [true]: ", pos))
+    print(paste0("Number of samples [false]: ", neg))
+    print(paste0("Total Number of samples: ", tot))
+    prop <- pos/tot * 100
+    print(paste0("Proportion of true samples: ", round(prop, digits = 1), "%" ))
+    print(paste0("Number signficantly DE: ", sum(deTable$adj.P.Val < pvalue & abs(deTable$logFC) >= foldchange))) 
+    print("*****************")
+    
+    geneTable <- mapToValidatedTargetGenes(deTable)
+    foutstring <- add_date_tag(str_join(outdir, "/DE_MIRNA_", dayString[day], "_", targetString, "_val_targets"), ".txt")
+    write.table(geneTable, quote=F, file=foutstring, sep="\t")
   }
-  print("Completed: miRNA Target identification")
 }
-
-# Differential Expression Analysis: Around Labor Day (Day 0) to (Day 4) of Sample Collection --------------
-print("Differential Expression Analysis: Around Labor Day (Day 0) to (Day 4) of Sample Collection --------------")
-#clin.rowname = rownames(Clin.dat)['desired trait']
-#traits        = vector of matching items wanted (e.g. blood draw date ranges from -31  to 309 days. To get days 1 and 2, 
-bloodDrawDates <- "N:M:CLIN:Data:Date_of_Blood_Collection__relative_to_Date_of_Birth"
-molDat_blood_draw_0_4 <- data_subset_Clin(Mol.dat.frame = molDat, Clin.dat.frame = clinDat, clin.rowname =bloodDrawDates , traits = c(0:4))
-
-print(paste0("The number of samples for blood draw days 0:4 is: ", dim(molDat_blood_draw_0_4)[[2]]))
-      
-resultsLocation <- "/Volumes/~nchambwe/inova/projects/gamcop/gamcop_pipeline/results/diffExpAnalysis/20150216/blood_draw_0_4/"
-
-sapply(gamcop, function(x) diffExprFun(clinMat = clinDat, dataMat=molDat_blood_draw_0_4, targetPheno=x, 
-                                       covarVec=covariates, FCThresh=foldChange, pValueThresh=pvalue, 
-                                       writingDir=resultsLocation))
-
-diffExpTables <- list.files(path = resultsLocation, pattern="DE_MIRNA_*", all.files = F, recursive = T)
-print(diffExpTables)
-
-
-for(i in 1:length(gamcop)){
-  sigMirsTable <- filter_diffExp(pathToDeTable =paste0(resultsLocation, diffExpTables[i]), FCThresh = foldChange, pValueThresh = pvalue)
-  mirs <- row.names(sigMirsTable)
-  comparison <- unlist(str_split(gamcop[i], pattern = ":"))[[5]]
-  print(paste0("The number of differentially expressed mirs for ", comparison, " is: ", length(mirs)))
-  
-  if(length(mirs)>=1){
-    filename <- paste0(resultsLocation, "sigMirList_", diffExpTables[i])
-    write.table(x = mirs, file = filename, quote = F, col.names = F,row.names=F)
-    get_validated_mir_targets(mirOfInterest = mirs, targetPhenotypeName =comparison , 
-                              writingDir =resultsLocation)
-  }
-  print("Completed: miRNA Target identification")
-}
-
-# Differential Expression Analysis: Day 1 of Sample Collection --------------
-print("Differential Expression Analysis: Day 1 of Sample Collection --------------")
-#clin.rowname = rownames(Clin.dat)['desired trait']
-#traits        = vector of matching items wanted (e.g. blood draw date ranges from -31  to 309 days. To get days 1 and 2, 
-bloodDrawDates <- "N:M:CLIN:Data:Date_of_Blood_Collection__relative_to_Date_of_Birth"
-molDat_blood_draw_1 <- data_subset_Clin(Mol.dat.frame = molDat, Clin.dat.frame = clinDat, clin.rowname =bloodDrawDates , traits = c(1))
-covariates = c("N:F:SURV:Data:Date_of_Birth__relative_to_Date_of_Birth", "C:F:ADMX:Data:Admix_80_Percent") 
-
-print(paste0("The number of samples for blood draw day 1 is: ", dim(molDat_blood_draw_1)[[2]]))
-
-resultsLocation <- "/Volumes/~nchambwe/inova/projects/gamcop/gamcop_pipeline/results/diffExpAnalysis/20150216/blood_draw_1/"
-
-sapply(gamcop, function(x) diffExprFun(clinMat = clinDat, dataMat=molDat_blood_draw_1, targetPheno=x, 
-                                       covarVec=covariates, FCThresh=foldChange, pValueThresh=pvalue, 
-                                       writingDir=resultsLocation))
-
-diffExpTables <- list.files(path = resultsLocation, pattern="DE_MIRNA_*", all.files = F, recursive = T)
-print(diffExpTables)
-
-
-for(i in 1:length(gamcop)){
-  sigMirsTable <- filter_diffExp(pathToDeTable =paste0(resultsLocation, diffExpTables[i]), FCThresh = foldChange, pValueThresh = pvalue)
-  mirs <- row.names(sigMirsTable)
-  filename <- paste0(resultsLocation, "sigMirList_", diffExpTables[i])
-  write.table(x = mirs, file = filename, quote = F, col.names = F,row.names=F)   
-  comparison <- unlist(str_split(gamcop[i], pattern = ":"))[[5]]
-  print(paste0("The number of differentially expressed mirs for ", comparison, " is: ", length(mirs)))
-  
-  if(length(mirs)>=1){
-     get_validated_mir_targets(mirOfInterest = mirs, targetPhenotypeName =comparison , 
-                              writingDir =resultsLocation)
-  }
-  print("Completed: miRNA Target identification")
-}
-
-# Quick summarize ptb status and number of samples
-#day0_4 <- clinDat[targetPhenotypes, names(molDat_blood_draw_0_4)]
-#table(t(day0_4["B:NB:CLIN:Critical_Phenotype:Preterm",]))
-#day1 <- clinDat[targetPhenotypes, names(molDat_blood_draw_1)]
-#table(t(day1["B:NB:CLIN:Critical_Phenotype:Preterm",]))
-#table(t(clinDat["B:NB:CLIN:Critical_Phenotype:Preterm", names(molDat)]))
-
